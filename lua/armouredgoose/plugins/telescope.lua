c5391c7f02ce1290d370b0f3f8241c7ad88ee22a
Koliras <gyrmanchyk230506@gmail.com>
2024-08-23 21:45:32 +0000

    add ignoring directories to telescope

--- lua/armouredgoose/plugins/telescope.lua
+++ lua/armouredgoose/plugins/telescope.lua
@@ -32,6 +32,10 @@ return {
 					require("telescope.themes").get_dropdown(),
 				},
 			},
+			file_ignore_patterns = {
+				"node%_modules/.*",
+				".git",
+			},
 		})
 
 		-- Enable Telescope extensions if they are installed
