fcf3b34c9216ccb7ad38ef1ee0a1dcc0b3c02fed
Koliras <gyrmanchyk230506@gmail.com>
2024-09-02 19:04:45 +0000

    add fugitive

--- lua/armouredgoose/plugins/git.lua
+++ lua/armouredgoose/plugins/git.lua
@@ -1,12 +1,17 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-	'lewis6991/gitsigns.nvim',
-	opts = {
-		signs = {
-			add = { text = '+' },
-			change = { text = '~' },
-			delete = { text = '_' },
-			topdelete = { text = '‾' },
-			changedelete = { text = '~' },
+	{
+		'tpope/vim-fugitive',
+	},
+	{
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
 		},
 	},
 }
