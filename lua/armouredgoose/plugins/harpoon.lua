8de8a8e134d87ff006140769b460be3be0a3051c
Koliras <gyrmanchyk230506@gmail.com>
2024-11-04 19:30:47 +0200

    change harpoon bindings

--- lua/armouredgoose/plugins/harpoon.lua
+++ lua/armouredgoose/plugins/harpoon.lua
@@ -6,16 +6,28 @@ return {
 		local harpoon = require("harpoon")
 		harpoon:setup({
 			settings = {
-				save_on_toggle = true
-			}
+				save_on_toggle = true,
+			},
 		})
 
-		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
-		vim.keymap.set("n", "<C-c>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-x>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-z>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<C-\\>", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "¢", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "»", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "«", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "|", function()
+			harpoon:list():select(4)
+		end)
 	end,
 }
