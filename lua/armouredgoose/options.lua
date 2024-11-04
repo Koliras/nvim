f9938ed4039d82b1902908613d4c25423a9f0e65
Koliras <gyrmanchyk230506@gmail.com>
2024-11-04 19:30:09 +0200

    make better folding

--- lua/armouredgoose/options.lua
+++ lua/armouredgoose/options.lua
@@ -52,3 +52,6 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevelstart = 99
