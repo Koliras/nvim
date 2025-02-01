992d20e5d22a9ebcfbb12a994c57d0d5eae2f895
Koliras <gyrmanchyk230506@gmail.com>
2025-02-01 11:45:23 +0200

    change theme

--- lua/armouredgoose/plugins/colorscheme.lua
+++ lua/armouredgoose/plugins/colorscheme.lua
@@ -1,18 +1,54 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"folke/tokyonight.nvim",
+	lazy = false,
 	priority = 1000,
+	opts = {},
 	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			transparent_background = true,
+		require("tokyonight").setup({
+			style = "moon",
+			transparent = true,
+			light_style = "day", -- The theme is used when the background is set to light
+			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
 			styles = {
 				comments = {},
-				conditionals = {},
+				keywords = {},
+				functions = {},
+				variables = {},
+				-- Background styles. Can be "dark", "transparent" or "normal"
+				sidebars = "transparent", -- style for sidebars, see below
+				floats = "transparent", -- style for floating windows
+			},
+			day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+			dim_inactive = false, -- dims inactive windows
+			lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+			--- You can override specific color groups to use other groups or a hex color
+			--- function will be called with a ColorScheme table
+			---@param colors ColorScheme
+			on_colors = function(colors) end,
+
+			--- You can override specific highlights to use other groups or a hex color
+			--- function will be called with a Highlights and ColorScheme table
+			---@param highlights tokyonight.Highlights
+			---@param colors ColorScheme
+			on_highlights = function(highlights, colors) end,
+
+			cache = true, -- When set to true, the theme will be cached for better performance
+
+			---@type table<string, boolean|{enabled:boolean}>
+			plugins = {
+				-- enable all plugins when not using lazy.nvim
+				-- set to false to manually enable/disable plugins
+				all = package.loaded.lazy == nil,
+				-- uses your plugin manager to automatically enable needed plugins
+				-- currently only lazy.nvim is supported
+				auto = true,
+				-- add any plugins here that you want to enable
+				-- for all possible plugins, see:
+				--   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+				telescope = true,
 			},
 		})
-
-		-- setup must be called before loading
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("tokyonight")
 	end,
 }
