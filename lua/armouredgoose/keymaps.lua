b629c2948d9a24783e2f88482ddf23e822a5fd92
Koliras <gyrmanchyk230506@gmail.com>
2025-02-04 19:37:52 +0200

    add oil nvim

--- lua/armouredgoose/keymaps.lua
+++ lua/armouredgoose/keymaps.lua
@@ -6,8 +6,6 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagn
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
-vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
