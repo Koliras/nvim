f8a16ffd8d613051d497c79e4ef8761baf54a1fc
Koliras <gyrmanchyk230506@gmail.com>
2025-02-09 00:55:46 +0200

    remove zellij related plugins since they are not needed

--- lua/armouredgoose/plugins/plugins.lua
+++ lua/armouredgoose/plugins/plugins.lua
@@ -1,14 +1,3 @@
 return {
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-	{
-		"https://github.com/fresh2dev/zellij.vim",
-		-- Pin version to avoid breaking changes.
-		-- tag = '0.3.*',
-		lazy = false,
-		init = function()
-			-- Options:
-			-- vim.g.zelli_navigator_move_focus_or_tab = 1
-			-- vim.g.zellij_navigator_no_default_mappings = 1
-		end,
-	},
 }
