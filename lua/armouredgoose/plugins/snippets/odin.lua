cdd00fc57aab75d0e32769f8635348ea0dfe78f9
Koliras <gyrmanchyk230506@gmail.com>
2025-03-08 22:38:18 +0200

    add proc snippet besides fn

--- lua/armouredgoose/plugins/snippets/odin.lua
+++ lua/armouredgoose/plugins/snippets/odin.lua
@@ -6,15 +6,18 @@ local text = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
 
+local proc_snippet = {
+	i(1, "foo"),
+	text({ " :: proc(" }),
+	i(2),
+	text({ ") " }),
+	c(3, { snip_n(nil, { text("-> "), i(1, "int"), text(" ") }), text("") }),
+	text({ "{", "\t" }),
+	i(0),
+	text({ "", "}" }),
+}
+
 return {
-	snip("fn", {
-		i(1, "foo"),
-		text({ " :: proc(" }),
-		i(2),
-		text({ ") " }),
-		c(3, { snip_n(nil, { text("-> "), i(1, "int"), text(" ") }), text("") }),
-		text({ "{", "\t" }),
-		i(0),
-		text({ "", "}" }),
-	}),
+	snip("fn", proc_snippet),
+	snip("proc", proc_snippet),
 }
