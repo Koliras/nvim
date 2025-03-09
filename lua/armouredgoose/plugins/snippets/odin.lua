f9724ec14e85932f093b8b0128c1a2cd96fdbd40
Koliras <gyrmanchyk230506@gmail.com>
2025-03-09 21:06:08 +0200

    fix snippets for procs in odin

--- lua/armouredgoose/plugins/snippets/odin.lua
+++ lua/armouredgoose/plugins/snippets/odin.lua
@@ -6,20 +6,27 @@ local text = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
 
-local proc_snippet = {
-	i(1, "foo"),
-	text({ " :: proc(" }),
-	i(2),
-	text({ ") " }),
-	c(3, { snip_n(nil, { text("-> "), i(1, "int"), text(" ") }), text("") }),
-	text({ "{", "\t" }),
-	i(0),
-	text({ "", "}" }),
-}
-
 return {
-	snip("fn", proc_snippet),
-	snip("proc", proc_snippet),
+	snip("fn", {
+		i(1, "foo"),
+		text({ " :: proc(" }),
+		i(2),
+		text({ ") " }),
+		c(3, { snip_n(nil, { text("-> "), i(1, "int"), text(" ") }), text("") }),
+		text({ "{", "\t" }),
+		i(0),
+		text({ "", "}" }),
+	}),
+	snip("proc", {
+		i(1, "foo"),
+		text({ " :: proc(" }),
+		i(2),
+		text({ ") " }),
+		c(3, { snip_n(nil, { text("-> "), i(1, "int"), text(" ") }), text("") }),
+		text({ "{", "\t" }),
+		i(0),
+		text({ "", "}" }),
+	}),
 	snip("main", {
 		text({ "main :: proc() {", "\t" }),
 		i(0),
