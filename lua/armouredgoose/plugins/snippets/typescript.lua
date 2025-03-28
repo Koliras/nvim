899cdcd1dc0f25ede0b2dc30b60d0d02c43e9ce2
Koliras <gyrmanchyk230506@gmail.com>
2025-03-28 19:34:17 +0200

    resok

--- lua/armouredgoose/plugins/snippets/typescript.lua
+++ lua/armouredgoose/plugins/snippets/typescript.lua
@@ -5,8 +5,20 @@ local snip_n = ls.snippet_node
 local text = ls.text_node
 local i = ls.insert_node
 local c = ls.choice_node
+local f = ls.function_node
+
+local function copy(args)
+	return args[1]
+end
 
 return {
+	snip("resok", {
+		text({ "if (!" }),
+		i(1, "response"),
+		text({ ".ok) {", "\tthrow new Error(" }),
+		f(copy, 1),
+		text({ ".statusText)", "}" }),
+	}),
 	snip("fn", {
 		text({ "function " }),
 		i(1, "foo"),
